ec8c9e3346c77987ca43a31b8907b79fb583125e
denizbutengineer <deniznezirdundar@gmail.com>
2025-04-25 11:26:57 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import enigma.console.TextWindow;
 import enigma.console.TextWindowNotAvailableException;
 import enigma.core.Enigma;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.PrintWriter;
@@ -12,9 +15,23 @@ import java.io.Reader;
 
 public class Main {
     public static void main(String[] args) {
-      enigma.console.Console console = Enigma.getConsole("Snake Game");
+      enigma.console.Console cn = Enigma.getConsole("Snake Game");
+        
+        try {
+        	BufferedReader reader = new BufferedReader(new FileReader("maze.txt"));
+        	String s = reader.readLine();
+        	while(!s.equals(" ")) {
+        		System.out.println(s);
+        		s = reader.readLine();
+        	}
+        	reader.close();
+        } catch(IOException e) {
+        	e.printStackTrace();
+        }
+        
+        cn.getTextWindow().setCursorPosition(58, 2);
         InputQueue inputQueue=new InputQueue();
-        console.getTextWindow().output(inputQueue.writeElements());
+        cn.getTextWindow().output(inputQueue.writeElements());
 
     }
 }
