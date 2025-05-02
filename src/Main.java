8f7e22fa317c279528f185948603029f68268ffe
davudeminkarauzum <davudeminkarauzum@hotmail.com>
2025-05-02 11:31:12 +0300

    Deneme

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,41 @@ public class Main {
     public static void main(String[] args) {
       enigma.console.Console console = Enigma.getConsole("Snake Game");
         InputQueue inputQueue=new InputQueue();
+<<<<<<< Updated upstream
         console.getTextWindow().output(inputQueue.writeElements());
 
+=======
+        
+        cn.getTextWindow().setCursorPosition(58, 2);
+  	    cn.getTextWindow().output(inputQueue.writeElements());
+            
+        for(int i = 0; i < 30; i++) { // oyunun başında inputQueue'dan 30 element boşalt
+    	  
+    	  boolean isInserted = false;
+    	  
+    	  while(!isInserted) {//deneme cart curt
+    	
+    	  int x = random.nextInt(23);  
+    	  int y = random.nextInt(55); // rastgele koordinat belirlenmesi 
+    	  
+    	  if(mazeMap[x][y] == ' ') { // koordinat boşsa 
+    		  
+    		  Object next = inputQueue.dequeueInput(); 
+              char c = next.toString().charAt(0);
+        
+              cn.getTextWindow().setCursorPosition(y, x); 
+              cn.getTextWindow().output(c);  // o koordinata inputQueue elemanını yazdır
+              
+              mazeMap[x][y] = c; // elementleri bir daha aynı yere eklememek için mazeMap'i de güncellemeliyiz
+              
+              isInserted = true; // element başarıyla yerleşti
+              
+              cn.getTextWindow().setCursorPosition(58, 2);
+        	  cn.getTextWindow().output("                 "); // Queue üst üste yazılmasın diye önce boşluk koy
+           	  cn.getTextWindow().setCursorPosition(58, 2);
+        	  cn.getTextWindow().output(inputQueue.writeElements()); // Queue'yu tekrar yazdır
+    	  }
+    	  
+>>>>>>> Stashed changes
     }
 }
