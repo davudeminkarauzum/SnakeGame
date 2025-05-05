4cc486d16f58dda9a920c883ee2da3660399c6ca
cme1252delisi <alp3rsakar@yandex.com>
2025-05-05 17:37:19 +0300

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Random;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import enigma.event.TextMouseEvent;
