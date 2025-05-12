5da199f2a8c7ff999f5dacbf08e0acd513a71035
raiju43 <selahattinacikgoz8@gmail.com>
2025-05-12 11:49:13 +0300

    s nakeleri için GameFielnd update

--- src/GameField.java
+++ src/GameField.java
@@ -1,13 +1,11 @@
-
-import enigma.console.TextAttributes;
 import java.awt.Color;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
+import enigma.console.TextAttributes;
 
-import enigma.core.Enigma;
 
 public class GameField {
 
@@ -15,6 +13,8 @@ public class GameField {
 
     public static char[][] map = new char[23][80]; // map arrayini oyun parametrelerini de göstermesi için genişlettim.
     public static enigma.console.Console cn;
+
+    private static Game mainGameReference;
 
     public GameField(enigma.console.Console console)
     {
@@ -36,8 +36,13 @@ public class GameField {
         } catch(IOException e) {
             e.printStackTrace();
         }
+
     }
-   
+
+
+    public boolean isWall(int x, int y) {
+        return map[x][y] == '#';
+    }
 
     public static void printScreen() {
         for (int i = 0; i < 23; i++) {
@@ -46,75 +51,47 @@ public class GameField {
 
                 TextAttributes attrs;
 
-                if(j < 55) {
-                    if (ch == 'P' && j < 55) {
-                        attrs = new TextAttributes(Color.BLACK, Color.BLUE);
-                    } else if (ch == 'C' && j < 55) {
-                        attrs = new TextAttributes(Color.BLACK, Color.GREEN);
-                    } else if(ch == 'S') {
-                    	attrs = new TextAttributes(Color.BLACK, Color.RED);
-                    } else if (ch == '#') {
-                        attrs = new TextAttributes(Color.WHITE, Color.BLACK); // duvar: mavi arka plan
-                    } else {
-                        attrs = new TextAttributes(Color.BLACK, Color.WHITE); // varsayılan
-                    }
+                if (ch == 'P' && j < 55) {
+                    attrs = new TextAttributes(Color.GREEN, Color.BLACK);
+                } else if (ch == 'C' && j < 55) {
+                    attrs = new TextAttributes(Color.RED, Color.BLACK);
+                } else if (ch == '#') {
+                    attrs = new TextAttributes(Color.WHITE, Color.BLUE); // duvar: mavi arka plan
                 } else {
-                	attrs = new TextAttributes(Color.WHITE, Color.BLACK); // varsayılan
+                    attrs = new TextAttributes(Color.WHITE, Color.BLACK); // varsayılan
                 }
-                cn.getTextWindow().output(j, i, ch, attrs); 
+
+                cn.getTextWindow().output(j, i, ch, attrs);
             }
         }
     }
-
-
-    public boolean isWall(int x, int y) {
-        return map[x][y] == '#';
-    }
-    
-    public boolean isStuck(int x, int y) {
-        return isWall(x + 1, y) || isWall(x - 1, y) || isWall(x, y + 1) || isWall(x, y - 1) ;
-    }
-    
-    public static boolean isCrashedComputer(int x, int y) {
-        return map[x][y] == 'C';
-    }
-    
-    public static boolean isCrashedS(int x, int y) {
-        return map[x][y] == 'S';
-    }
-    
-    public boolean isCrashedRobots(int x, int y) {
-    	return isCrashedComputer(x, y) || isCrashedS(x, y);
-    }
-    
-    
 
     public void unloadInputQueue() {
         Random random = new Random();
-
         boolean isInserted = false;
-
+        char c = ' ';
         while (!isInserted) {
-            int x = random.nextInt(23);
-            int y = random.nextInt(55); // rastgele koordinat belirlenmesi
+             int x = random.nextInt(23);
+             int y = random.nextInt(55); // rastgele koordinat belirlenmesi
 
             if (map[x][y] == ' ') { // koordinat boşsa
                 Object next = inputQueue.dequeueInput();
-                char c = next.toString().charAt(0);
+                c = next.toString().charAt(0);
 
-                map[x][y] = c; // elementleri bir daha aynı yere eklememek için mazeMap'i de güncellemeliyiz
-                
-                if(c == 'S') { 
-                	Game.robotS[Game.robotSCounter] = new SingleLinkedList();
-                    Game.robotS[Game.robotSCounter].add(c);
-                    Game.robotSX[Game.robotSCounter] = x;     Game.robotSY[Game.robotSCounter] = y;
-                	Game.robotSCounter++;
-                }
+                map[x][y] = c;
+
                 isInserted = true;
 
-                inputQueue.writeElements(); // Queue'yu tekrar yazdır.
+                inputQueue.writeElements(); // Queue'yu tekrar yazdır
+            }
+            if (c == 'S'){
+                Game.addNewSnake(x,y);
             }
         }
+
     }
 
+    public boolean isCrashedRobots(int px, int i) {
+        return false;
+    }
 }
