62c6a92f26de4d69fda9b560149655a32658b55f
cme1252delisi <alp3rsakar@yandex.com>
2025-05-12 17:45:02 +0300

    güncel gamefield

--- src/GameField.java
+++ src/GameField.java
@@ -1,11 +1,13 @@
+
+import enigma.console.TextAttributes;
 import java.awt.Color;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
-import enigma.console.TextAttributes;
 
+import enigma.core.Enigma;
 
 public class GameField {
 
@@ -13,7 +15,7 @@ public class GameField {
 
     public static char[][] map = new char[23][80]; // map arrayini oyun parametrelerini de göstermesi için genişlettim.
     public static enigma.console.Console cn;
-
+    
     private static Game mainGameReference;
 
     public GameField(enigma.console.Console console)
@@ -36,13 +38,8 @@ public class GameField {
         } catch(IOException e) {
             e.printStackTrace();
         }
-
     }
-
-
-    public boolean isWall(int x, int y) {
-        return map[x][y] == '#';
-    }
+   
 
     public static void printScreen() {
         for (int i = 0; i < 23; i++) {
@@ -51,21 +48,54 @@ public class GameField {
 
                 TextAttributes attrs;
 
-                if (ch == 'P' && j < 55) {
-                    attrs = new TextAttributes(Color.GREEN, Color.BLACK);
-                } else if (ch == 'C' && j < 55) {
-                    attrs = new TextAttributes(Color.RED, Color.BLACK);
-                } else if (ch == '#') {
-                    attrs = new TextAttributes(Color.WHITE, Color.BLUE); // duvar: mavi arka plan
-                } else {
-                    attrs = new TextAttributes(Color.WHITE, Color.BLACK); // varsayılan
+                if(j < 55) {
+                    if (ch == 'P' && j < 55) {
+                        attrs = new TextAttributes(Color.BLACK, Color.BLUE);
+                    } else if (ch == 'C' && j < 55) {
+                        attrs = new TextAttributes(Color.BLACK, Color.GREEN);
+                    } else if(ch == 'S') {
+                    	attrs = new TextAttributes(Color.BLACK, Color.RED);
+                    } else if (ch == '#') {
+                        attrs = new TextAttributes(Color.WHITE, Color.BLACK); // duvar: mavi arka plan
+                    } 
+                    else if (ch == '=') {
+                        attrs = new TextAttributes(Color.BLACK, Color.GREEN); // duvar: mavi arka plan
+                    }
+                    else if (ch == 'X') {
+                        attrs = new TextAttributes(Color.BLACK, Color.WHITE); // duvar: mavi arka plan
+                    }
+                    else {
+                        attrs = new TextAttributes(Color.BLACK, Color.WHITE); // varsayılan
+                    }
+                }   else {
+                	attrs = new TextAttributes(Color.WHITE, Color.BLACK); // varsayılan
                 }
-
-                cn.getTextWindow().output(j, i, ch, attrs);
+                cn.getTextWindow().output(j, i, ch, attrs); 
             }
         }
     }
 
+
+    public boolean isWall(int x, int y) {
+        return map[x][y] == '#';
+    }
+    
+    public boolean isStuck(int x, int y) {
+        return isWall(x + 1, y) || isWall(x - 1, y) || isWall(x, y + 1) || isWall(x, y - 1) ;
+    }
+    
+    public static boolean isCrashedComputer(int x, int y) {
+        return map[x][y] == 'C';
+    }
+    
+    public static boolean isCrashedS(int x, int y) {
+        return map[x][y] == 'S';
+    }
+    
+    public boolean isCrashedRobots(int x, int y) {
+    	return isCrashedComputer(x, y) || isCrashedS(x, y);
+    }
+     
     public void unloadInputQueue() {
         Random random = new Random();
         boolean isInserted = false;
@@ -91,7 +121,4 @@ public class GameField {
 
     }
 
-    public boolean isCrashedRobots(int px, int i) {
-        return false;
-    }
 }
