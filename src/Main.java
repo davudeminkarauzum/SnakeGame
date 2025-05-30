cba581fd4c6797d879747de8d58f82e21dfb6a1f
denizbutengineer <deniznezirdundar@gmail.com>
2025-05-30 15:52:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,5 @@
-import enigma.console.Console;
-import enigma.console.TextAttributes;
-import enigma.console.TextWindow;
-import enigma.console.TextWindowNotAvailableException;
-import enigma.core.Enigma;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.Reader;
-import java.util.Random;
-
-
 public class Main {
     public static void main(String[] args) throws Exception {
      Game game=new Game();
-   
     }
 }
