ca5f7a450429aaf3ce419709bc5159f84d8f272e
cme1252delisi <alp3rsakar@yandex.com>
2025-06-03 02:05:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,3 @@
-import enigma.console.Console;
-import enigma.console.TextAttributes;
-import enigma.console.TextWindow;
-import enigma.console.TextWindowNotAvailableException;
-import enigma.core.Enigma;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.Reader;
-
 
 public class Main {
     public static void main(String[] args) throws Exception {
