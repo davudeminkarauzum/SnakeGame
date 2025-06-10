2063b01f4365fcb2ca9e8ff6ae4c5685004337ce
denizbutengineer <deniznezirdundar@gmail.com>
2025-06-10 07:14:14 +0300

    Create Trap.java

---
+++ src/Trap.java
@@ -0,0 +1,75 @@
+public class Trap {
+	
+	private int x;
+	private int y;
+	private int time;
+	
+	Trap(int x, int y, int time) {
+		
+		this.x = x;
+		this.y = y;
+		this.time = time;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+	
+	
+	public boolean checkSnake() {
+		
+		boolean control = false;
+		
+		for(int i = -1; i <= 1; i++) {
+			for(int j = -1; j <= 1; j++) {
+				
+				if(GameField.map[x + i][y + j] == 'S') 
+					control = true;								
+			}	
+		}
+		
+		return control;
+		
+	}
+	
+	public void boom() {
+		
+		if(checkSnake()) {
+			
+			for(int i = -1; i <= 1; i++) {
+				for(int j = -1; j <= 1; j++) {
+					
+					if(GameField.map[x + i][y + j] != '#' && GameField.map[x + i][y + j] != 'P')
+					GameField.map[x + i][y + j] = 'x'; 
+				}	
+			}
+			
+		}
+	}
+	
+	public void disappear() {
+		
+		GameField.map[x][y] = ' ';	
+		time = -1;
+	}
+}
