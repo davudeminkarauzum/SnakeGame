36a6d19b3298cd117b85b672766d34c31176ef96
denizbutengineer <deniznezirdundar@gmail.com>
2025-06-12 16:49:31 +0300

    Update SingleLinkedList.java

--- src/SingleLinkedList.java
+++ src/SingleLinkedList.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class SingleLinkedList {
 
    private Node head;
@@ -32,18 +34,132 @@ public class SingleLinkedList {
 		   }
 	   }
 	   
+	   SnakeElement element = (SnakeElement) temp.getData();
+	   int oldX = element.getX(), oldY = element.getY();
+ 
+	   Node temp2 = snake.getHead();
+	   while(temp2.getLink() != null) {
+		   element = (SnakeElement) temp2.getData();
+		   int x = element.getX(), y = element.getY();
+		   Random random = new Random();
+           int randomx = 0, randomy = 0;
+
+           int lastmove1 = 4;
+
+           if (!(GameField.map[x][y + 1] == '#' || GameField.map[x][y + 1] == 'P'
+        	   || GameField.map[x][y + 1] == 'P' || Snake.isCrashedSnake(x, y + 1))) {
+               randomy = 1;
+           } else if (!(GameField.map[x][y - 1] == '#' || GameField.map[x][y - 1] == 'P'
+            	   || GameField.map[x][y - 1] == 'P' || Snake.isCrashedSnake(x, y - 1))) {
+               randomy = -1;
+           } else if (!(GameField.map[x - 1][y] == '#' || GameField.map[x - 1][y] == 'P'
+            	   || GameField.map[x - 1][y] == 'P' || Snake.isCrashedSnake(x - 1, y))) {
+               randomx = -1;
+           } else if (!(GameField.map[x + 1][y] == '#' || GameField.map[x + 1][y] == 'P'
+            	   || GameField.map[x + 1][y] == 'P' || Snake.isCrashedSnake(x + 1, y))) {
+               randomx = 1;
+           } else {
+
+               do {
+                   randomx = 0;
+                   randomy = 0;
+
+                   lastmove1 = random.nextInt(4);
+                   switch (lastmove1) {
+
+                       case (0): {
+                           randomx = -1;
+                           break;
+                       }
+                       case (1): {
+                           randomx = 1;
+                           break;
+                       }
+                       case (2): {
+                           randomy = -1;
+                           break;
+                       }
+                       case (3): {
+                           randomy = 1;
+                           break;
+                       }
+                   }
+               } while (GameField.map[x + randomx][y + randomy] == '#' || GameField.map[x + randomx][y + randomy] == 'P'
+                	   || GameField.map[x + randomx][y + randomy] == 'C' || Snake.isCrashedSnake(x + randomx, y + randomy));
+           }
+           
+           element.setX(oldX + randomx);
+           element.setY(oldY + randomy);
+           
+           oldX = element.getX();
+           oldY = element.getY();
+		   temp2 = (Node) temp2.getLink();
+	   }
+	
 	   Node afterTemp = null;
 	   if(temp.getLink() != null) {
 		   afterTemp = (Node) temp.getLink();
 	   }
 	   
-	   Node temp2 = snake.getHead();
-	   while(temp2.getLink() != null) {
-		   temp2 = (Node) temp2.getLink();
-	   }
-	
 	   if(afterTemp != null) {
 		   temp2.setLink(afterTemp);
+		   while(afterTemp != null) {
+			   element = (SnakeElement) temp2.getData();
+			   int x = element.getX(), y = element.getY();
+			   Random random = new Random();
+	           int randomx = 0, randomy = 0;
+
+	           int lastmove1 = 4;
+
+	           if (!(GameField.map[x][y + 1] == '#' || GameField.map[x][y + 1] == 'P'
+	        	   || GameField.map[x][y + 1] == 'P' || Snake.isCrashedSnake(x, y + 1))) {
+	               randomy = 1;
+	           } else if (!(GameField.map[x][y - 1] == '#' || GameField.map[x][y - 1] == 'P'
+	            	   || GameField.map[x][y - 1] == 'P' || Snake.isCrashedSnake(x, y - 1))) {
+	               randomy = -1;
+	           } else if (!(GameField.map[x - 1][y] == '#' || GameField.map[x - 1][y] == 'P'
+	            	   || GameField.map[x - 1][y] == 'P' || Snake.isCrashedSnake(x - 1, y))) {
+	               randomx = -1;
+	           } else if (!(GameField.map[x + 1][y] == '#' || GameField.map[x + 1][y] == 'P'
+	            	   || GameField.map[x + 1][y] == 'P' || Snake.isCrashedSnake(x + 1, y))) {
+	               randomx = 1;
+	           } else {
+
+	               do {
+	                   randomx = 0;
+	                   randomy = 0;
+
+	                   lastmove1 = random.nextInt(4);
+	                   switch (lastmove1) {
+
+	                       case (0): {
+	                           randomx = -1;
+	                           break;
+	                       }
+	                       case (1): {
+	                           randomx = 1;
+	                           break;
+	                       }
+	                       case (2): {
+	                           randomy = -1;
+	                           break;
+	                       }
+	                       case (3): {
+	                           randomy = 1;
+	                           break;
+	                       }
+	                   }
+	               } while (GameField.map[x + randomx][y + randomy] == '#' || GameField.map[x + randomx][y + randomy] == 'P'
+	                	   || GameField.map[x + randomx][y + randomy] == 'C' || Snake.isCrashedSnake(x + randomx, y + randomy));
+	           }
+	           
+	           element.setX(oldX + randomx);
+	           element.setY(oldY + randomy);
+	           
+	           oldX = element.getX();
+	           oldY = element.getY();
+			   afterTemp = (Node) afterTemp.getLink();
+		   }
 	   }
 	   snake.getHead().setLink(temp);
    }
@@ -57,7 +173,6 @@ public class SingleLinkedList {
 	   SnakeElement element = (SnakeElement) temp.getData();
 	   int headX = element.getX(), headY = element.getY();
 	   Snake snake = new Snake(headX, headY);
-	   Game.snakes.enqueue(snake);
 	   
 	   temp = (Node) temp.getLink();
 	   while(temp != null) {
@@ -67,6 +182,8 @@ public class SingleLinkedList {
 		   temp = (Node) temp.getLink();
 	   }
 	   
+	   Game.snakes.enqueue(snake);
+	   snake.displaySnakeOnMap();
 	   temp = head;
 	   for(int i = 0; i < count; i++) {
 		   temp = (Node) temp.getLink();
@@ -139,10 +256,10 @@ public class SingleLinkedList {
 		   SnakeElement bodyPart = (SnakeElement) temp.getData();
 		   int x = bodyPart.getX(), y = bodyPart.getY();
 		   GameField.map[x][y] = ' ';
-		   bodyPart.setElement(' '); bodyPart.setX(' '); bodyPart.setY(' ');
+		   bodyPart.setElement(' '); bodyPart.setX(0); bodyPart.setY(0);
 		   temp = (Node) temp.getLink();
 	   }
-	   
+	   GameField.map[0][0] = '#';
    }
    
    public int size() {
